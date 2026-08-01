bc0362489ff8864152d666f17d1452e54c48ee61
Yash Chandrani <chandraniyash11@gmail.com>
2026-07-31 23:03:13 -0400

    commited 3

--- Nexora-Backend/configs/db.js
+++ Nexora-Backend/configs/db.js
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 
 const db=async()=>{
     try {
-        await mongoose.connect(MONGODB_URL)
+        const mongoDbUrl = process.env.MONGODB_URL;
+
+        if (!mongoDbUrl) {
+            throw new Error("MONGODB_URL is not configured");
+        }
+
+        await mongoose.connect(mongoDbUrl)
         console.log("Database connected")
     } catch (error) {
         console.log(error)
